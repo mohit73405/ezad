57d40dd34e9dbc7c1520ac4d1e184acc9f661985
kiranPadwal <padwalkiran1985@gmail.com>
2018-08-15 02:11:46 +0200

    api updated

--- apis/youtube.py
+++ apis/youtube.py
@@ -6,6 +6,14 @@ import datetime
 
 ns_youtube = Namespace('Youtube', description='Youtube Apis')
 
+search_channels_form = ns_youtube.model('Search Channels', {
+    'category_id' : fields.String(required=False, description='Category ID'),
+    'country' : fields.String(required=False, description='Country'),
+    'min_lower' : fields.Integer(required=False, description='Min Followers'),
+    'max_upper' : fields.Integer(required=False, description='Max Followers'),
+    'sort_order' : fields.String(required=False, description='Sort Order')
+})
+
 
 @ns_youtube.route('/regionCodes')
 class RegionCodes(Resource):
@@ -30,4 +38,21 @@ class VideoCategories(Resource):
         connecsiObj = ConnecsiModel()
         video_categories = connecsiObj.get__(table_name='youtube_video_categories', STAR='*')
         return {'data': video_categories}
+
+@ns_youtube.route('/searchChannels/<string:channel>')
+class SearchChannels(Resource):
+    @ns_youtube.expect(search_channels_form)
+    def post(self,channel):
+        '''search channels'''
+        form_data = request.get_json()
+        category_id = form_data.get('category_id')
+        country = form_data.get('country')
+        min_lower = form_data.get('min_lower')
+        max_upper = form_data.get('max_upper')
+        sort_order = form_data.get('sort_order')
+        connecsiObj=ConnecsiModel()
+        data = connecsiObj.search_inf(channel_id=channel,
+                                      min_lower=str(min_lower), max_upper=str(max_upper)
+                                      , category_id=str(category_id), country=str(country), sort_order=sort_order)
+        return {'data':data}
 
