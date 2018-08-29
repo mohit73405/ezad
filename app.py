88f97e40d226c3d8bb0bba80b9b30cf2ce13a5af
kiranPadwal <padwalkiran1985@gmail.com>
2018-08-29 23:56:32 +0200

    api brand updated

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ from apis.campaign import ns_campaign
 from apis.messages import ns_messages
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 blueprint = Blueprint('api',__name__,url_prefix='/api')
 api = Api(blueprint,version='1.0', title='Connecsi Api',description='APIS',doc='/documentation')
 app.register_blueprint(blueprint)
 
 
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+# cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 api.namespaces.pop(0)
 
