ef41109e1ddfd6a240beb335441447cdc368fa3e
kiranPadwal <padwalkiran1985@gmail.com>
2018-10-26 18:37:03 +0200

    updated classified ads and added sql

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from apis.brand import ns_brand
 from apis.user import ns_user
 from apis.youtube import ns_youtube
 from apis.campaign import ns_campaign
+from apis.classified import ns_classified
 from apis.messages import ns_messages
 
 app = Flask(__name__)
@@ -25,6 +26,7 @@ api.add_namespace(ns_brand)
 api.add_namespace(ns_payments)
 api.add_namespace(ns_youtube)
 api.add_namespace(ns_campaign)
+api.add_namespace(ns_classified)
 api.add_namespace(ns_messages)
 
 
