f60f8f01ff6604ef6c3c1dae1f2b0453366df24e
kiranPadwal <padwalkiran1985@gmail.com>
2018-11-22 22:28:43 +0100

    updated bcr

--- model/ConnecsiModel.py
+++ model/ConnecsiModel.py
@@ -726,7 +726,7 @@ class ConnecsiModel:
     def update_brand_campaign_report(self,user_id,campaign_id,data):
         try:
             with self.cnx.cursor() as cursor:
-                sql = "UPDATE brand_campaign_report SET revenue_generated = '%s', currency = '%s', target_url = '%s'" \
+                sql = "UPDATE brand_campaign_report SET revenue_generated = '%s', currency = '%s', target_url = '%s'," \
                       " new_users = '%s' WHERE user_id = '"+ user_id + "'" +" AND campaign_id = '"+ campaign_id +"'"
                 print(sql)
                 cursor.execute(sql,data)
