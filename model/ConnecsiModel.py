fb353285b7e3b6b40f45e88c6fa619b3173e0fb8
kiranPadwal <padwalkiran1985@gmail.com>
2018-11-28 12:28:09 +0100

    updated inf edit profile api

--- model/ConnecsiModel.py
+++ model/ConnecsiModel.py
@@ -826,7 +826,7 @@ class ConnecsiModel:
                 #       " WHERE ccm.status = 'Current Partner' and cp.channel_id = '" + channel_id + "'"
 
                 sql = "SELECT " \
-                      " cp1.campaign_id,chm.youtube_channel_id,chm.twitter_channel_id,cp1.proposal_channels" \
+                      " cp1.campaign_id,chm.youtube_channel_id,chm.twitter_channel_id,cp1.proposal_channels," \
                       " chm.confirmed" \
                       " FROM channels_mapper chm" \
                       " LEFT JOIN campaign_proposal cp1 on cp1.channel_id = chm.youtube_channel_id" \
