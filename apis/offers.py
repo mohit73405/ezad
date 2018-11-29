e0fab05a2a61becc1de87aba684bc143117d27bf
kiranPadwal <padwalkiran1985@gmail.com>
2018-11-29 12:53:03 +0100

    updated inf offers api

--- apis/offers.py
+++ apis/offers.py
@@ -183,8 +183,8 @@ class Offers(Resource):
                        'regions',
                        'min_lower_followers', 'max_upper_followers', 'files', 'video_cat_id',
                        'offer_description',
-                       'arrangements', 'kpis', 'no_of_views', 'no_of_replies', 'mapped_yotube_channel_id',
-                       'mapped_twitter_channel_id', 'confirmed']
+                       'arrangements', 'kpis', 'no_of_views', 'no_of_replies'
+                       ]
             response_list = []
             for item in offer_data:
                 dict_temp = dict(zip(columns, item))
