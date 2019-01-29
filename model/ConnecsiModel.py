6d71aafecefed6316775293c54e56cdd952f087f
kiranPadwal <padwalkiran1985@gmail.com>
2019-01-29 07:00:21 +0100

    search inf offer

--- model/ConnecsiModel.py
+++ model/ConnecsiModel.py
@@ -581,11 +581,11 @@ class ConnecsiModel:
                 if channel_name:
                     conditions.append(" io.channels LIKE '%"+channel_name +"%'")
                 if category_id:
-                    conditions.append(" io.video_cat_id LIKE '%"+category_id+"%'")
+                    conditions.append(" io.video_cat_id LIKE '"+category_id+"'")
                 if country:
-                    conditions.append(" io.country LIKE '%" + category_id + "%'")
+                    conditions.append(" io.country LIKE '%" + country + "%'")
                 if arrangements:
-                    conditions.append(" io.arrangements LIKE '%"+category_id+"%'")
+                    conditions.append(" io.arrangements LIKE '%"+arrangements+"%'")
                 if min_lower and max_upper:
                     conditions.append(" io.min_lower_followers >= " +min_lower+ " AND io.max_upper_followers <= " + max_upper )
                 if currency:
