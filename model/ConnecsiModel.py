0f2d1ede710a9c3a37788ca4a417955be8532236
kiranPadwal <padwalkiran1985@gmail.com>
2019-02-04 03:43:00 +0100

    inf campaigns details api

--- model/ConnecsiModel.py
+++ model/ConnecsiModel.py
@@ -1164,7 +1164,7 @@ class ConnecsiModel:
             with self.cnx.cursor() as cursor:
                 sql = "SELECT " \
                       " cp.campaign_id,bc.campaign_name,bc.campaign_status,bc.user_id,ub.first_name,ub.last_name," \
-                      " bc.regions,ub.profile_pic,ub.email_ic," \
+                      " bc.regions,ub.profile_pic,ub.email_id," \
                       " cp.proposal_id,cp.proposal_from_date, cp.proposal_to_date," \
                       " cp.currency,cp.proposal_price," \
                       " cp.proposal_description,cp.proposal_arrangements,cp.proposal_kpis,cp.target_url,cp.ref_link," \
