9109fa8591afb3cb269b528d12431af3e31a69a5
kiranPadwal <padwalkiran1985@gmail.com>
2019-03-01 21:15:35 +0100

    total videos

--- model/ConnecsiModel.py
+++ model/ConnecsiModel.py
@@ -58,7 +58,7 @@ class ConnecsiModel:
                 "t1.facebook_url,t1.insta_url,t1.twitter_url,t1.country " \
                 "FROM youtube_channel_details t1 " \
                 "INNER JOIN youtube_channel_ids_video_categories_id t2 on t1.channel_id = t2.channel_id " \
-                "WHERE t1.subscriberCount_gained BETWEEN "+min_lower+ " AND " + max_upper
+                "WHERE t1.subscriberCount_gained != 0 AND  t1.subscriberCount_gained BETWEEN "+min_lower+ " AND " + max_upper
                 # "left join youtube_channel_ids_regioncode t3 on t1.channel_id = t3.channel_id " \
 
                 if category_id and country:
@@ -191,7 +191,7 @@ class ConnecsiModel:
 
                 sql = "SELECT distinct(t1.channel_id) FROM youtube_channel_details t1 " \
                       "INNER JOIN youtube_channel_ids_video_categories_id t2 on t1.channel_id = t2.channel_id " \
-                      "WHERE t1.subscriberCount_gained BETWEEN " + min_lower + " AND " + max_upper
+                      "WHERE t1.subscriberCount_gained != 0 AND t1.subscriberCount_gained BETWEEN " + min_lower + " AND " + max_upper
                 # "left join youtube_channel_ids_regioncode t3 on t1.channel_id = t3.channel_id " \
 
                 if category_id and country:
