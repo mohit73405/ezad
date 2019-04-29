647519344e43c2f59c1e9ac8e0ea8d1dd2cc1c22
kiranPadwal <padwalkiran1985@gmail.com>
2019-04-29 03:32:45 +0200

    country api

--- model/ConnecsiModel.py
+++ model/ConnecsiModel.py
@@ -1447,9 +1447,9 @@ class ConnecsiModel:
                       " JOIN users_influencers ui on ui.channel_id = chm.youtube_channel_id or ui.channel_id=chm.twitter_channel_id" \
                       " JOIN youtube_channel_details ycd on ycd.channel_id = chm.youtube_channel_id" \
                       " JOIN twitter_channel_details tcd on tcd.twitter_id = chm.twitter_channel_id" \
-                      " JOIN youtube_channel_ids_video_categories_id ycivc on ycd.channel_id = ycivc.channel_id" \
+                      " JOIN youtube_channel_ids_video_categories_id ycivc on ycd.channel_id = ycivc.channel_id or chm.youtube_channel_id = ycivc.channel_id" \
                       " JOIN youtube_video_categories yvc on yvc.video_cat_id = ycivc.video_cat_id" \
-                      " WHERE ui.channel_id = '" + user_id + "' and ycd.channel_id = '"+user_id+"' and ycivc.channel_id = '"+user_id+"'"
+                      " WHERE ui.channel_id = '" + user_id + "' and ycd.channel_id = '"+user_id+"'"
 
 
                 # sql = " select ui1.first_name as youtube_first_name,ui2.first_name as twitter_first_name,chm.youtube_channel_id," \
