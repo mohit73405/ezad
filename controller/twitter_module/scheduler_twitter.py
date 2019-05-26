9100e04fed9b19af69e1b07110d679c40971a4fe
kiranPadwal <padwalkiran1985@gmail.com>
2019-05-26 20:20:15 +0200

    twitter api

--- controller/twitter_module/scheduler_twitter.py
+++ controller/twitter_module/scheduler_twitter.py
@@ -52,43 +52,65 @@ def get_data_by_screen_name():
             screen_name = ''
             output=''
             try:
-
-                print(item[1])
+                print('twitter url = ',item[1])
                 vc_data = modelObj.get_youtube_categories_by_channel_id(channel_id=channel_id)
                 print(vc_data)
-            except:pass
+            except Exception as e:
+                print(e)
+                pass
             video_categories=[]
-            try:
-                for vc_item in vc_data:
-                    video_categories.append(vc_item[1])
-            except:pass
+            if vc_data:
+                try:
+                    for vc_item in vc_data:
+                        video_categories.append(vc_item[1])
+                except Exception as e:
+                    print(e)
+                    print('VC DATA IS EMPTY')
+                    pass
             try:
                 twitter_url = item[1]
                 # output = re.findall('http(.*)', twitter_url)
-            except:pass
+            except Exception as e:
+                print(e)
+                pass
             try:
                 parsed = urllib.parse.urlsplit(twitter_url)
                 path=parsed.path
                 output=path.rsplit('/', 3)
-            except:pass
+            except Exception as e:
+                print(e)
+                pass
             try:
                 screen_name = output[1]
                 print(screen_name)
-            except:pass
+            except Exception as e:
+                print(e)
+                pass
 
             try:
                 conObj = TwitterApiController()
+                print('GETTING DATA FROM API = ', channel_id)
                 conObj.get_data_by_screen_name(channel_id=channel_id,twitter_url=twitter_url,screen_name=screen_name,video_categories=video_categories
                                                    ,country=country,facebook_url=facebook_url,insta_url=insta_url,youtube_url=youtube_url)
-            except:pass
+            except Exception as e:
+                print(e)
+                pass
 
         else:
             try:
+                print('DONT HAVE TWITTER URL = ',channel_id)
                 # data = [channel_id, 'false']
                 # modelObj.insert__(table_name='channels_mapper',columns=['youtube_channel_id','confirmed'],data=data)
                 modelObj.insert_into_channels_mapper(youtube_channel_id=channel_id,confirmed='false')
-            except:pass
-        modelObj.insert__(table_name='youtube_channel_ids_done_for_twitter',columns=['channel_id'],IGNORE='IGNORE',data=[channel_id])
+            except Exception as e:
+                print(e)
+                pass
+        print('INSERTING INTO DONE TABLE = ',channel_id)
+        try:
+            modelObj.insert__(table_name='youtube_channel_ids_done_for_twitter',columns=['channel_id'],IGNORE='IGNORE',data=[channel_id])
+        except Exception as e:
+            print(e)
+            pass
 
 
 def get_content_categories():
