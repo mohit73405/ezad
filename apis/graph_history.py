536d80afd7230143e0adc450ced4c4b46d220e7e
kiranPadwal <padwalkiran1985@gmail.com>
2019-06-11 03:12:44 +0200

    graph youtube api

--- apis/graph_history.py
+++ apis/graph_history.py
@@ -14,7 +14,7 @@ class History(Resource):
         """get no of followers history by channel name and id"""
         modelObj = ConnecsiModel()
         if channel_name == 'youtube':
-            columns = ['channel_id', 'subscriberCount_gained', 'inserted_time']
+            columns = ['channel_id', 'subscriberCount_gained', 'inserted_date']
             channel_history_data = modelObj.get__(table_name='youtube_channels_history', columns=columns,WHERE='WHERE'
                                           ,compare_column='channel_id',compare_value=str(channel_id))
             response_list = []
