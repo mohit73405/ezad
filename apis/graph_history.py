c1a33e952d7c25d295bc2d7d99515275be17d596
kiranPadwal <padwalkiran1985@gmail.com>
2019-07-09 00:42:01 +0200

    graph api

--- apis/graph_history.py
+++ apis/graph_history.py
@@ -35,4 +35,48 @@ class History(Resource):
                 response_list.append(dict_temp)
             return {'data': response_list}
 
+        if channel_name == 'twitter':
+            columns = ['twitter_id', 'no_of_followers', 'inserted_date']
+            channel_history_data = modelObj.get__(table_name='twitter_channels_history', columns=columns,WHERE='WHERE'
+                                          ,compare_column='twitter_id',compare_value=str(channel_id))
+            response_list = []
+            response_columns = ['twitter_id','no_of_followers','timestamp']
+            print(channel_history_data)
+            # channel_history_data = list(channel_history_data)
+            channel_history_data_list = []
+            for item in channel_history_data:
+                temp_list = []
+                temp_list.append(item[0])
+                temp_list.append(item[1])
+                timestamp = datetime.datetime.timestamp(item[2])
+                temp_list.append(timestamp)
+                print('temp list = ',temp_list)
+                channel_history_data_list.append(temp_list)
+            for item1 in channel_history_data_list:
+                dict_temp = dict(zip(response_columns, item1))
+                response_list.append(dict_temp)
+            return {'data': response_list}
+
+        if channel_name == 'instagram':
+            columns = ['insta_id', 'no_of_followers', 'inserted_date']
+            channel_history_data = modelObj.get__(table_name='insta_channels_history', columns=columns,WHERE='WHERE'
+                                          ,compare_column='insta_id',compare_value=str(channel_id))
+            response_list = []
+            response_columns = ['insta_id','no_of_followers','timestamp']
+            print(channel_history_data)
+            # channel_history_data = list(channel_history_data)
+            channel_history_data_list = []
+            for item in channel_history_data:
+                temp_list = []
+                temp_list.append(item[0])
+                temp_list.append(item[1])
+                timestamp = datetime.datetime.timestamp(item[2])
+                temp_list.append(timestamp)
+                print('temp list = ',temp_list)
+                channel_history_data_list.append(temp_list)
+            for item1 in channel_history_data_list:
+                dict_temp = dict(zip(response_columns, item1))
+                response_list.append(dict_temp)
+            return {'data': response_list}
+
 
