77c7fb0ca7fe491784d6f84512c01fa864cc41dd
kiranPadwal <padwalkiran1985@gmail.com>
2019-07-30 17:53:16 +0200

    subscription apis

--- model/ConnecsiModel.py
+++ model/ConnecsiModel.py
@@ -2671,7 +2671,7 @@ class ConnecsiModel:
             with self.cnx.cursor() as cursor:
                 print(data)
                 insert_sql = "INSERT INTO subscriptions_for_brands (user_id, feature_name, units, price,customized_feature) " \
-                      " VALUES(%s,%s,%s,%s,%s)"
+                      " VALUES('%s',%s,'%s','%s',%s)"
                 select_sql = "SELECT 1 from subscriptions_for_brands where user_id ="+data[0]+" and feature_name = '"+data[1]+"'"
                 update_sql = "UPDATE subscriptions_for_brands SET units="+data[2]+" ,price= "+data[3]+" , customized_feature='"+data[4]+"'" \
                              " WHERE user_id ="+data[0]+" and feature_name = '"+data[1]+"'"
