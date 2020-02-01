e7a43e29cce7fa6b69d4d1cb458e8ffe771acbd6
kiranPadwal <padwalkiran1985@gmail.com>
2020-02-01 18:09:30 +0100

    inbox api

--- model/ConnecsiModel.py
+++ model/ConnecsiModel.py
@@ -3216,10 +3216,13 @@ class ConnecsiModel:
                       " m.deleted_from_user_id,m.deleted_from_bin_user_id,m.read, " \
                       " ub.first_name as brand_first_name,ub.profile_pic as brand_profile_pic, " \
                       " ui.first_name as inf_first_name, ycd.channel_img as inf_youtube_profile_pic " \
+                      " bc.campaign_id,bc.campaign_name " \
                       " FROM conversations m " \
                       " JOIN users_brands ub ON ub.email_id = m.conv_from_email_id OR ub.email_id = m.conv_to_email_id " \
                       " JOIN youtube_channel_details ycd ON ycd.business_email = m.conv_from_email_id OR ycd.business_email = m.conv_to_email_id " \
                       " JOIN users_influencers ui ON ui.business_email = m.conv_from_email_id OR ui.business_email = m.conv_to_email_id " \
+                      " JOIN channel_campaign_message ccm ON ccm.message_id = m.message_id " \
+                      " JOIN brands_campaigns bc ON bc.campaign_id = ccm.campaign_id " \
                       " WHERE m.message_id = '"+message_id+"'"
                 print(sql)
                 cursor.execute(sql)
